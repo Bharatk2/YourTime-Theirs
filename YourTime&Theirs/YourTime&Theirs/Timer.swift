2d1e1838ec61f3c421a008139e16c12009278f36
bharatk2 <bharatiosdeveloper@gmail.com>
2020-10-29 13:44:41 -0700

    Added Timer Model

---
+++ YourTime&Theirs/YourTime&Theirs/Timer.swift
@@ -0,0 +1,17 @@
+//
+//  Timer.swift
+//  YourTime&Theirs
+//
+//  Created by Bharat Kumar on 10/29/20.
+//  Copyright © 2020 Bharat Kumar. All rights reserved.
+//
+
+import Foundation
+
+struct Timer: Codable, Equatable {
+    var country: String?
+    var timeZone: String?
+    var currentTime: Date?
+    var locationTime: Date?
+    var timeLeft: Date?
+}
